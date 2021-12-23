7bc0881c74965eb521a05b42b029920fbf517cff
emirhanersoz <63202294+emirhanersoz@users.noreply.github.com>
2021-12-23 23:53:08 +0300

    Modeller Oluşturuldu
    Modeller Oluşturuldu.

---
+++ Web Programlama Projesi/eBilet/eBilet/Models/Aktör.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace eBilet.Models
+{
+    public class Aktör
+    {
+        [Key]
+        public int Id { get; set; }
+        public string ProfilResmiURL { get; set; }
+        public string AdSoyad { get; set; }
+        public string Bio { get; set; }
+
+    }
+}
---
+++ Web Programlama Projesi/eBilet/eBilet/Models/Film.cs
@@ -0,0 +1,24 @@
+﻿using eBilet.Data;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace eBilet.Models
+{
+    public class Film
+    {
+        [Key]
+        public int Id { get; set; }
+        public string İsim { get; set; }
+        public string Açıklama { get; set; }
+        public double Fiyat { get; set; }
+        public string ResimURL { get; set; }
+        public DateTime BaşlangıçTarihi { get; set; }
+        public DateTime BitişTarihi { get; set; }
+        public FilmKategori FilmKategori { get; set; }
+
+
+    }
+}
