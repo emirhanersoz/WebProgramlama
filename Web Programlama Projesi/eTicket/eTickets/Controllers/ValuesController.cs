12bcc5b4a165e86d0b254359abac471f203890f6
emirhanersoz <63202294+emirhanersoz@users.noreply.github.com>
2021-12-28 17:24:39 +0300

    eTickets
    Projemizin son olarak bitmiş hali

---
+++ Web Programlama Projesi/eTicket/eTickets/Controllers/ValuesController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace eTickets.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ValuesController : ControllerBase
+    {
+        public string Get()
+        {
+            return "Merhabalar WebAPi";
+        }
+    }
+}
